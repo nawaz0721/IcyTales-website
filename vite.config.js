f61bc7d55d748280f79bbffa0efe50b46b2758f3
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2024-10-21 01:53:34 +0500

    build error 3

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,12 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
+export default {
   build: {
+    chunkSizeWarningLimit: 1000, // Adjusts the warning threshold for chunk sizes
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "react-vendor";
-            }
-            if (id.includes("lodash")) {
-              return "lodash";
-            }
-            return "vendor";
-          }
-          if (id.includes("src/pages")) {
-            return "pages";
-          }
-          if (id.includes("src/components")) {
-            return "components";
-          }
+        manualChunks: {
+          vendor: ["react", "react-dom", "lodash"], // Separates these libraries into their own chunk
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
   },
-  // Add resolve options for module resolution
-  resolve: {
-    alias: {
-      '@context': '/src/context', // Optional: Set up a path alias for easier imports
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure Vite resolves .jsx files automatically
-  },
-});
+};
